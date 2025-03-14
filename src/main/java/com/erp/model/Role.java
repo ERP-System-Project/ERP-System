7282a5b6c0d0b6ea50f8cce8c757cae5dae811f6
Mostafa Mohamed <mostafa.mosael13@gmail.com>
2025-03-14 17:58:34 +0200

    added user and role entities

---
+++ src/main/java/com/erp/model/Role.java
@@ -0,0 +1,26 @@
+package com.erp.model;
+import java.util.Set;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "roles")  
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+}
